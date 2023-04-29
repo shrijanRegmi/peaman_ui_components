0b72308f2697c9c4a1783d3d376d167e413045d3
Shrijan Regmi <your.email@example.com>
2023-04-29 09:24:00 +0545

    app setup complete for ios

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,16 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:peaman_ui_components/peaman_ui_components.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Peaman.initializeApp();
 
-  runApp(
-    const PeamanApp(title: 'Peaman'),
+  final result = await Peaman.initializeApp();
+  result.when(
+    (success) => runApp(
+      const PeamanApp(title: 'Peaman'),
+    ),
+    (failure) => log(failure.message),
   );
 }
