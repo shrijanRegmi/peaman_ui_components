4073281b7a932ef762ba1555f8cae15e18022004
Shrijan Regmi <your.email@example.com>
2023-12-14 21:31:24 +0545

    feat: fvm implemented; updated gitignore

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,14 @@ import Foundation
 import cloud_firestore
 import cloud_functions
 import facebook_auth_desktop
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
 import flutter_secure_storage_macos
-import path_provider_macos
+import path_provider_foundation
 import sqflite
 import url_launcher_macos
 
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
